b061cc14ffa84bdb844cec011808e39e1532ed99
Dan <lordaniel24@gmail.com>
2023-08-12 17:16:07 -0500

    window

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,45 @@
 #include <GLFW/glfw3.h>
 
 
-
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
 int main ( int argc, char* argv[] ) {
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD\n";
+        return EXIT_FAILURE;
+    }
+
+    glViewport(0, 0, 800, 600);
+   
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+
+    glfwTerminate();
     std::cout << "Goodbye Cruel World!\n";
-    return 0;
+    return EXIT_SUCCESS;
 }
