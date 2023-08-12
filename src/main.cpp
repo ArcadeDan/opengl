b1706129ae295bd50735cffa2f73f5762eb7b4ab
Dan <lordaniel24@gmail.com>
2023-08-12 16:42:16 -0500

    cmake

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "../include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+
+
+
+int main ( int argc, char* argv[] ) {
+    std::cout << "Goodbye Cruel World!\n";
+    return 0;
+}
